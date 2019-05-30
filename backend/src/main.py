a96b8c08ddc3128c4ed47c3feca72c77ab7662e8
Kevin Summersill <ksummersill@appddictionstudio.com>
2019-05-30 12:47:46 -0500

    updated api and chartjs

--- backend/src/main.py
+++ backend/src/main.py
@@ -112,12 +112,83 @@ def get_avg_sector_rates():
     df.loc[df['MRKCAP_TYPE'] == 'None', 'Conversion'] = df['MRKCAP_AMT'].fillna(value='0')
     df['Conversion'] = df['Conversion'].fillna(value='0')
     df['Conversion'] = df['Conversion'].astype(int)
-    res = df.groupby(['SEC_CONV'])['Conversion'].mean().reset_index()
-    res = res.to_json(orient='columns')
+    res = df.groupby(['SEC_CONV'])['Conversion'].mean().reset_index(drop=True, inplace=True)
+    res = res.to_json(orient='recordss')
     res = json.dumps(res)
     res = res.replace('\\','')
     res = res.replace('"{','{')
     res = res.replace('}"','}')
+    return res
+
+def tokmb (num):
+    convNum = ''
+    if (num >= 1000 and num < 1000000):
+        num = round (num/1000.00,2)
+        round (num, 2)
+        convNum = str(num)
+        #  + " K"
+        return convNum
+    elif (num >= 1000000 and num < 1000000000):
+        num = round (num/1000000.00,2)
+        round (num, 2)
+        convNum = str(num)
+        #  + " M"
+        return convNum
+    elif (num >= 1000000000 and num < 1000000000000):
+        num = round (num/1000000000.00,2)
+        convNum = str(num)
+        #  + " B"
+        return convNum
+
+def get_avg_sector_rates_abbrev():
+    # sub = ''
+    cols_to_use = [0,1,2,3,4,5,6,7]
+    csv_file_path = r'../assets/companylist.csv'
+    df = pd.read_csv(csv_file_path, 
+        usecols= cols_to_use,
+        encoding='utf-8'
+        )
+    df['IPOyear'] = df['IPOyear'].astype(str)
+    df['IPOyear'] = df['IPOyear'].str.strip('.0')
+    df = df.rename(index=str, columns = {"Symbol": "SYM", "Name": "CO.", "LastSale": "LP", "MarketCap": "MRKCAP", "IPOyear": "YR", "Sector": "SEC", "industry": "IND", "Summary Quote": "SUMQTE"})
+    df['SYM'] = df['SYM'].str.strip()
+    df['SEC'] = df['SEC'].str.strip()
+    df['SEC_CONV'] = np.where(df['SEC']=="Consumer Durables", 'Consumer Industry', df['SEC'])
+    df['SEC_CONV'] = np.where(df['SEC']=="Consumer Non-Durables", 'Consumer Industry', df['SEC'])
+    df['SEC_CONV'] = np.where(df['SEC']=='Consumer Services', 'Consumer Industry', df['SEC'])
+    df["SEC"] = df["SEC"].replace("Empty", "No Industry Identified")
+    df["SEC_CONV"] = df["SEC_CONV"].replace("Empty", "No Industry Identified")
+    df["SEC_CONV"] = df["SEC_CONV"].replace("Consumer Durables", "Consumer Industry")
+    df["SEC_CONV"] = df["SEC_CONV"].replace("Consumer Non-Durables", "Consumer Industry")
+    df["SEC_CONV"] = df["SEC_CONV"].replace("Technology", "Technology/Energy")
+    df["SEC_CONV"] = df["SEC_CONV"].replace("Energy", "Technology/Energy")
+    df = df.groupby('SEC_CONV').head(100)
+    df = df.iloc[:, [8,3]]
+    df['MRKCAP'] = df['MRKCAP'].str.replace('$', '')
+    df['MRKCAP'] = df['MRKCAP'].str.replace(',', '')
+    df['MRKCAP'] = df['MRKCAP'].str.replace('M', ' M')
+    df['MRKCAP'] = df['MRKCAP'].str.replace('B', ' B')
+    df['SEC_CONV'] = df['SEC_CONV'].fillna(value='No Industry Identified')
+    df['MRKCAP'] = df['MRKCAP'].fillna(value='0 D')
+    df.dropna(inplace = True)
+    df[['MRKCAP_AMT','MRKCAP_TYPE']] = df.MRKCAP.str.split(" ",expand=True,)
+    millions = df['MRKCAP_AMT'].str.replace('.', '').fillna(value='0').astype(int).multiply(other = 100, fill_value = 5)
+    billions = df['MRKCAP_AMT'].str.replace('.', '').fillna(value='0').astype(int).multiply(other = 1000000, fill_value = 5)
+    df.loc[df['MRKCAP_TYPE'] == 'M', 'Conversion'] = millions
+    df.loc[df['MRKCAP_TYPE'] == 'B', 'Conversion'] = billions
+    df.loc[df['MRKCAP_TYPE'] == 'D', 'Conversion'] = 0
+    df.loc[df['MRKCAP_TYPE'] == 'None', 'Conversion'] = df['MRKCAP_AMT'].fillna(value='0')
+    df['Conversion'] = df['Conversion'].fillna(value='0')
+    df['Conversion'] = df['Conversion'].astype(int)
+    res = df.groupby(['SEC_CONV'])['Conversion'].mean().reset_index()
+    
+    for x in range (0, len(res['Conversion'])):
+        res ['Conversion'][x] = tokmb(res ['Conversion'][x])
+    res = res.to_json(orient='records')
+    #res = json.dumps(res)
+    #res = res.replace('\\','')
+    #res = res.replace('"{','{')
+    #res = res.replace('}"','}')
     return res
 
 def get_distinct_sector_listing():
@@ -175,7 +246,11 @@ def get_industries_by_sector():
     df = json.dumps(df.values.tolist())
     # df.replace('\\','')
     return df
-get_avg_sector_rates()
+
+@app.route('/getAvgSectorRatesAbr')
+def abbrev_sector():
+    return jsonify(get_avg_sector_rates_abbrev())
+
 @app.route('/getAvgSectorRates')
 def get_avg_sector():
     return jsonify(get_avg_sector_rates())
