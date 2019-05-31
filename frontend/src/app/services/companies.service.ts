03bc9ab24e50e8f99ed5a1d18263feac6ff66669
Hussain Alseddiq <alseddiq@gmail.com>
2019-05-31 05:08:20 -0700

    Adding service to get news

--- frontend/src/app/services/companies.service.ts
+++ frontend/src/app/services/companies.service.ts
@@ -10,23 +10,28 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class CompanyService {
-    constructor(private http: HttpClient) {
-    }
-  
- // GET list of public
-  getCompanies(){
-    return this.http.get(`${environment.API_URL}/companyAnalysis`)
+  constructor(private http: HttpClient) {}
+
+  // GET list of public
+  getCompanies() {
+    return this.http.get(`${environment.API_URL}/companyAnalysis`);
   }
 
-  getDistinctSector(){
-    return this.http.get(`${environment.API_URL}/distinctSectors`)
+  getDistinctSector() {
+    return this.http.get(`${environment.API_URL}/distinctSectors`);
   }
 
-  getSectorAndIndustryData(){
-    return this.http.get(`${environment.API_URL}/distinctSectorsWithIndustries`)
+  getSectorAndIndustryData() {
+    return this.http.get(
+      `${environment.API_URL}/distinctSectorsWithIndustries`
+    );
   }
 
   getSectorAvgs() {
-    return this.http.get(`${environment.API_URL}/getAvgSectorRatesAbr`)
+    return this.http.get(`${environment.API_URL}/getAvgSectorRatesAbr`);
+  }
+
+  getNews() {
+    return this.http.get(`${environment.API_URL}/news`);
   }
 }
